6e370117cb3181365382d9729649e3dcfb4435d8
Arne Coucheron <arco68@gmail.com>
2020-11-07 00:46:10 +0100

    soong: Add flag for disabling postrender cleanup
    Some legacy devices experiences bad lag unless this is disabled.
    
    Disable it with:
    TARGET_DISABLE_POSTRENDER_CLEANUP := true
    
    Change-Id: I2b40dc4cbcab06fb293d1f407321af2fae846a51

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -33,6 +33,7 @@ SOONG_CONFIG_NAMESPACES += corvusGlobalVars
 SOONG_CONFIG_corvusGlobalVars += \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
+    disable_postrender_cleanup \
     has_legacy_camera_hal1 \
     ignores_ftp_pptp_conntrack_failure \
     needs_netd_direct_connect_rule \
@@ -74,6 +75,7 @@ SOONG_CONFIG_corvusQcomVars_supports_hw_fde_perf := $(TARGET_HW_DISK_ENCRYPTION_
 SOONG_CONFIG_corvusQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_corvusQcomVars_uses_qcom_bsp_legacy := $(TARGET_USES_QCOM_BSP_LEGACY)
 SOONG_CONFIG_corvusQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
+SOONG_CONFIG_corvusGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
