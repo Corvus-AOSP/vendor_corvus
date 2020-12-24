PixelExperience/vendor_aosp

vendor: Disable Google Play System Updates activity if apex disabled
Change-Id: If30c3b29e61026521078f0ed75ffb34ef8f8dcc7

config/common.mk
@@ -188,7 +188,16 @@ PRODUCT_PRODUCT_PROPERTIES += \
 #endif
 
 # Apex
+ifeq ($(TARGET_FLATTEN_APEX),false)
 $(call inherit-product, vendor/aosp/config/apex.mk)
+else
+# Hide "Google Play System Updates" if Apex disabled
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    vendor/aosp/overlay_apex_disabled
+
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/aosp/overlay_apex_disabled/common
+endif
 
 # Audio
 $(call inherit-product, vendor/aosp/config/audio.mk)

Corvus-AOSP/vendor_corvus

config/common.mk
@@ -113,5 +113,13 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
 
 # Apex
+ifeq ($(TARGET_FLATTEN_APEX),false)
 $(call inherit-product, vendor/corvus/config/apex.mk)
+else
+# Hide "Google Play System Updates" if Apex disabled
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    vendor/corvus/overlay_apex_disabled
 
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/corvus/overlay_apex_disabled/common
+endif