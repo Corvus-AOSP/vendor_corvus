03dd43b9b2201933cc9ec4127b15d4971cdea0ca
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-02-07 15:37:23 -0300

    vendor: Add back FaceUnlock support
    Change-Id: I361ce913dfb5f1db574b3cf3cdeeeaaca2338ac1

--- config/common.mk
+++ config/common.mk
@@ -119,3 +119,14 @@ ifneq (,$(filter $(RAVEN_LAIR), Official Beta-Official))
          $(error Official build can't be done without signing keys; exiting)
     endif
 endif
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
