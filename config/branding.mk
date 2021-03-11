56be8c3293c58f25223313ab4bdbb51e42751198
StarkDroid <gamerprince.exp@gmail.com>
2021-03-11 19:01:04 +0530

    Bump version to 15.0

--- config/branding.mk
+++ config/branding.mk
@@ -1,7 +1,7 @@
 # Set all versions
 RAVEN_LAIR ?= Unofficial
-CORVUS_VERSION_BASE := 14.5
-CORVUS_CODENAME := Mutate
+CORVUS_VERSION_BASE := 15.0
+CORVUS_CODENAME := RavenClaw
 
 # Set all versions
 BUILD_DATE := $(shell date -u +%d%m%Y)
