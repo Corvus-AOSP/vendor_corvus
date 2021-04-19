39a6bb74f272afbc5ad7b756c557bf4a0ee25815
StarkDroid <gamerprince.exp@gmail.com>
2021-04-20 03:13:56 +0530

    Bump to 16.0
    Lets educate dem users with space
    
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>

--- config/branding.mk
+++ config/branding.mk
@@ -1,7 +1,7 @@
 # Set all versions
 RAVEN_LAIR ?= Unofficial
-CORVUS_VERSION_BASE := 15.0
-CORVUS_CODENAME := RavenClaw
+CORVUS_VERSION_BASE := 16.0
+CORVUS_CODENAME := Alpha-Centauri
 
 # Set all versions
 BUILD_DATE := $(shell date -u +%d%m%Y)
