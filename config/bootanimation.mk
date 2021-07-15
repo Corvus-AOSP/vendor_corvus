4c620c83a27acb9526e5b92f0d90cba2fa9ba73c
segfault2k <sebastian.sariego.benitez@gmail.com>
2021-07-15 19:15:43 -0400

    bootanimation: Add apollo

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -3,7 +3,7 @@
 
 corvus_device := $(patsubst %f,%,$(subst corvus_,,$(TARGET_PRODUCT)))
 
-ifneq ($(filter jd2019 kunlun2 begonia raphael enchilada lavender payton mido miatoll excalibur gram wayne whyred platina ginkgo vince tulip beryllium violet X00T X01BD RMX1801 tissot x2 jasmine_sprout sirius surya sakura grus kenzo davinci phoenix RMX1921 CPH1859 RMX2001 juice,$(corvus_device)),)
+ifneq ($(filter jd2019 kunlun2 begonia raphael enchilada lavender payton mido miatoll excalibur gram wayne whyred platina ginkgo vince tulip beryllium violet X00T X01BD RMX1801 tissot x2 jasmine_sprout sirius surya sakura grus kenzo davinci phoenix RMX1921 CPH1859 RMX2001 juice apollo,$(corvus_device)),)
 scr_resolution := 1080
 endif
 
