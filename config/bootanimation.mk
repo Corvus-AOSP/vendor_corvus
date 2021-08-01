187dea7cfb1cbee8516f7707cef2b647c2fc4d97
victor10520 <merser2005@gmail.com>
2021-08-01 22:15:25 +0300

    bootanimation: cleanup unsupported devices and refactor
    * Added support for common bootanimation flag
    
    Change-Id: I835a61bb7d4755166fa536da94a5e45a824c0539

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -3,11 +3,11 @@
 
 corvus_device := $(patsubst %f,%,$(subst corvus_,,$(TARGET_PRODUCT)))
 
-ifneq ($(filter s2 jd2019 kunlun2 begonia raphael enchilada lavender payton mido miatoll excalibur gram wayne whyred platina ginkgo vince tulip beryllium violet X00T X01BD RMX1801 tissot x2 jasmine_sprout sirius surya sakura grus kenzo davinci phoenix RMX1921 CPH1859 RMX2001 juice apollo,$(corvus_device)),)
+ifneq ($(filter s2 jd2019 kunlun2 begonia raphael lavender payton mido miatoll excalibur gram whyred ginkgo beryllium violet X00T surya sakura phoenix RMX1921 CPH1859 RMX2001 juice apollo,$(corvus_device)),) || ($(TARGET_BOOT_ANIMATION_RES),1080)
 scr_resolution := 1080
 endif
 
-ifneq ($(filter ysl riva land santoni X00P laurel_sprout,$(corvus_device)),)
+ifneq ($(filter ysl laurel_sprout,$(corvus_device)),) || ($(TARGET_BOOT_ANIMATION_RES),720)
 scr_resolution := 720
 endif
 
