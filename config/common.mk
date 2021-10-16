45319b9f0fb1bddff5fd8483837f283c2fba8df8
Vinay Verma <vvinay@codeaurora.org>
2021-10-16 05:27:57 +0200

    vendor: Disable Async MTE on System Server
    Async mode MTE is enabled by default on System Server process.
    Add property arm64.memtag.process.system_server=off
    This property is read during system server forking process and
    accordingly Heap Tagging level is set.
    
    Change-Id: I5ff384601f15ae5ef3dfcb6c02832843c6f2942c

--- config/common.mk
+++ config/common.mk
@@ -66,6 +66,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_SYSTEM_PROPERTIES += \
     persist.device_config.runtime_native_boot.iorap_perfetto_enable=true
 
+# Disable async MTE on system_server
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    arm64.memtag.process.system_server=off
+
 # Proton Clang
 ifeq ($(USE_PROTON),true)
 KERNEL_SUPPORTS_LLVM_TOOLS := true
