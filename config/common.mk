crdroidandroid/android_vendor_crdroid

config: DEVICE_PACKAGE_OVERLAYS -> PRODUCT_PACKAGE_OVERLAYS
 * DEVICE_PACKAGE_OVERLAYS corresponds to overlays that
   are device specific, these are not so they should
   belong in platform.

Change-Id: I8684e10a370ba1b9f0a725c0f199d167a98d1668
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

config/common.mk
@@ -189,7 +189,7 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay
-DEVICE_PACKAGE_OVERLAYS += vendor/lineage/overlay/common
+PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/common
 
 PRODUCT_VERSION_MAJOR = 11
 PRODUCT_VERSION_MINOR = 0

Corvus-AOSP/vendor_corvus

config/common.mk
@@ -128,7 +128,7 @@ include vendor/corvus/config/packages.mk
 
 # Overlays
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/corvus/overlay
-DEVICE_PACKAGE_OVERLAYS += vendor/corvus/overlay/common
+PRODUCT_PACKAGE_OVERLAYS += vendor/corvus/overlay/common
 
 # Copy all init rc files
 $(foreach f,$(wildcard vendor/corvus/prebuilt/common/etc/init/*.rc),\