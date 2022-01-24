062376b7b79fce780ca65cffed80206c597824ac
Trishiraj <gamerprince.exp@gmail.com>
2022-01-24 19:44:02 +0000

    Enable IORap
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>
    Change-Id: Ib7024078d5f4b102b5a4b7b9187de58c2d5e0eac

--- config/art.mk
+++ config/art.mk
@@ -11,3 +11,7 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
   SystemUI \
   Settings \
   Launcher3QuickStep
+
+# IORap app launch prefetching using Perfetto traces and madvise
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.iorapd.enable=true
