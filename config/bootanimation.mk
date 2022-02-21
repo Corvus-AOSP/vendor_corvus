d62934e8cdb8e0476e734c65417f8661a84d9fef
Roxor-007 <68855313+Roxor-007@users.noreply.github.com>
2022-02-21 00:35:05 -0800

    Add alioth

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -3,7 +3,7 @@
 
 corvus_device := $(patsubst %f,%,$(subst corvus_,,$(TARGET_PRODUCT)))
 
-ifneq ($(filter s2 jd2019 kunlun2 begonia raphael lavender payton mido miatoll excalibur gram whyred ginkgo beryllium violet X00T surya sakura phoenix RMX1921 CPH1859 RMX2001 juice apollo vayu kebab,$(corvus_device)),)
+ifneq ($(filter s2 jd2019 kunlun2 begonia raphael lavender payton mido miatoll excalibur gram whyred ginkgo beryllium violet X00T surya sakura phoenix RMX1921 CPH1859 RMX2001 juice apollo vayu kebab alioth,$(corvus_device)),)
 scr_resolution := 1080
 endif
 
