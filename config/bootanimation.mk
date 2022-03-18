a28d77e0fee1c540c0b5e44af008533eb1375f77
Trishiraj <gamerprince.exp@gmail.com>
2022-03-18 04:57:25 -0500

    Unify and keep only single instance of bootanimation
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>
    Change-Id: I5c821ad44c4c5b858f0206ba3f9391da7499b6ff

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -1,18 +1,4 @@
-
 # Boot Animation
-
-corvus_device := $(patsubst %f,%,$(subst corvus_,,$(TARGET_PRODUCT)))
-
-ifneq ($(filter s2 jd2019 kunlun2 begonia raphael lavender payton mido miatoll excalibur gram whyred ginkgo beryllium violet X00T surya sakura phoenix RMX1921 CPH1859 RMX2001 juice apollo vayu kebab alioth,$(corvus_device)),)
-scr_resolution := 1080
-endif
-
-ifneq ($(filter ysl gemini laurel_sprout,$(corvus_device)),)
-scr_resolution := 720
-endif
-
-ifneq ($(wildcard vendor/corvus/prebuilt/common/bootanimation/$(scr_resolution).zip),)
 PRODUCT_COPY_FILES += \
-    vendor/corvus/prebuilt/common/bootanimation/$(scr_resolution).zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-endif
+    vendor/corvus/prebuilt/common/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 
