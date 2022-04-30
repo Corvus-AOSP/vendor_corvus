749b4d6597289dcc55076c6e82a4d993f779d5a5
StarkDroid <gamerprince.exp@gmail.com>
2022-04-30 22:10:19 +0000

    branding: Format build date
    This will be directly used in our Corvus Version dialog.
    
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    Change-Id: I592479548be98c505650664cd025750c19801211

--- config/branding.mk
+++ config/branding.mk
@@ -4,10 +4,10 @@ CORVUS_VERSION_BASE := S2.1
 CORVUS_CODENAME := Revolt
 
 # Set all versions
-BUILD_DATE := $(shell date -u +%d%m)
+BUILD_DATE := $(shell date -u +%d-%m-%Y)
 BUILD_TIME := $(shell date -u +%H%M)
 BUILD_DATE_TIME := $(BUILD_TIME)$(BUILD_DATE)
-ROM_FINGERPRINT := Corvus/$(RAVEN_LAIR)/$(PLATFORM_VERSION)/$(BUILD_ID)/$(BUILD_DATE)/$(BUILD_TIME)
+ROM_FINGERPRINT := Corvus/$(RAVEN_LAIR)/$(PLATFORM_VERSION)/$(BUILD_ID)/$(shell date -u +%d%m)/$(BUILD_TIME)
 
 ifeq ($(USE_GAPPS), true)
     CORVUS_VERSION := Corvus_v$(CORVUS_VERSION_BASE)-$(CORVUS_CODENAME)-$(CORVUS_BUILD)-Gapps-$(RAVEN_LAIR)-$(BUILD_TIME)
