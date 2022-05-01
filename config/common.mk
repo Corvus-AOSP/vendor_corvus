107712a4032bfff70dbf714f6efec432b68e619c
StarkDroid <gamerprince.exp@gmail.com>
2022-05-01 21:19:52 +0000

    Revert "config: sepolicy: import flipendo/turbo_adapter sepolicy"
    This reverts commit a6ae0d9f424344d69574477b3a3bca0575781f53.

--- config/common.mk
+++ config/common.mk
@@ -170,6 +170,3 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Enable gestural navigation overlay to match default navigation mode
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural;com.google.android.systemui.gxoverlay
-
-# Sepolicy
-$(call inherit-product, vendor/corvus/config/sepolicy.mk)
