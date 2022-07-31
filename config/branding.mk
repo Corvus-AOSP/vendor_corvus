4affe79cac2dc44371de31d6b95d43012a52c9a1
CoolDude <103044605+CoolDudeRMX2020@users.noreply.github.com>
2022-07-31 17:48:28 +0200

    [1/2] config: Add official RMX2020 support

--- config/branding.mk
+++ config/branding.mk
@@ -3,7 +3,7 @@ RAVEN_LAIR := Unofficial
 CORVUS_VERSION_BASE := S3.2
 CORVUS_CODENAME := Vindicate
 
-ifneq ($(filter Trishiraj victor10520 KSSRAO ManavjitSingh MAdMiZ Fosslover Meghthedev PAiN Roxor-007 H3M3L AnhTran Sukeerat EinarG nathankits Ankan005 Ajit YadavMohit19 Ritzzz TheSanty,$(CORVUS_MAINTAINER)),)
+ifneq ($(filter Trishiraj victor10520 KSSRAO ManavjitSingh MAdMiZ Fosslover Meghthedev PAiN Roxor-007 H3M3L AnhTran Sukeerat EinarG nathankits Ankan005 Ajit YadavMohit19 Ritzzz TheSanty CoolDude6942,$(CORVUS_MAINTAINER)),)
 RAVEN_LAIR := Official
 endif
 
