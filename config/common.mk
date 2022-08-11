Evolution-X-AOSP/vendor_evolution

vendor: Smart Charging (4/4)
Change-Id: I51966875aaacc4b0068772ae732abab16652c973

config/common.mk
@@ -132,7 +132,8 @@ PRODUCT_COPY_FILES += \
 
 # Copy all Evolution X-specific init rc files
 PRODUCT_COPY_FILES += \
-    vendor/evolution/prebuilt/common/etc/init/init.evolution-x-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.evolution-x-updater.rc
+    vendor/evolution/prebuilt/common/etc/init/init.evolution-x-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.evolution-x-updater.rc \
+    vendor/evolution/prebuilt/common/etc/init/smartcharge-init.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/smartcharge-init.rc
 
 # Enable Android Beam on all targets
 PRODUCT_COPY_FILES += \

Corvus-AOSP/vendor_corvus

config/common.mk
@@ -88,7 +88,8 @@ PRODUCT_COPY_FILES += \
 
 # Permission
 PRODUCT_COPY_FILES += \
-    vendor/corvus/prebuilt/common/etc/permissions/corvus-power-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/corvus-power-whitelist.xml
+    vendor/corvus/prebuilt/common/etc/permissions/corvus-power-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/corvus-power-whitelist.xml \
+    vendor/corvus/prebuilt/common/etc/init/smartcharge-init.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/smartcharge-init.rc
 
 # Enable wireless Xbox 360 controller support
 PRODUCT_COPY_FILES += \
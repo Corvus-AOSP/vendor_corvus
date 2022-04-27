338c4877ec57c988c97898f2002f9191a7a0ffbc
DennySPB <dennyspb@gmail.com>
2022-04-27 17:43:13 +0300

    vendor: Smart Charging (4/4)
    Change-Id: I51966875aaacc4b0068772ae732abab16652c973

--- config/common.mk
+++ config/common.mk
@@ -88,7 +88,8 @@ PRODUCT_COPY_FILES += \
 
 # Permission
 PRODUCT_COPY_FILES += \
-    vendor/corvus/prebuilt/common/etc/permissions/corvus-power-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/corvus-power-whitelist.xml
+    vendor/corvus/prebuilt/common/etc/permissions/corvus-power-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/corvus-power-whitelist.xml \
+    vendor/corvus/prebuilt/common/etc/init/smartcharge-init.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/smartcharge-init.rc
 
 # Enable wireless Xbox 360 controller support
 PRODUCT_COPY_FILES += \
