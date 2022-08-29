adbaba2f4094c08921c9dc0eea5bcf8781932fc9
Michael Bestas <mkbestas@lineageos.org>
2022-08-30 02:39:47 +0300

    config: Remove TARGET_USES_QCOM_BSP_LEGACY
    Change-Id: I93f111285c46e1a1f811b9d8555de96474d60386

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -57,7 +57,6 @@ SOONG_CONFIG_corvusQcomVars += \
     legacy_hw_disk_encryption \
     supports_extended_compress_format \
     uses_pre_uplink_features_netmgrd \
-    uses_qcom_bsp_legacy \
     uses_qti_camera_device \
     needs_camera_boottime_timestamp
 
@@ -80,7 +79,6 @@ SOONG_CONFIG_corvusNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK
 SOONG_CONFIG_corvusQcomVars_legacy_hw_disk_encryption := $(TARGET_LEGACY_HW_DISK_ENCRYPTION)
 SOONG_CONFIG_corvusQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_corvusQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
-SOONG_CONFIG_corvusQcomVars_uses_qcom_bsp_legacy := $(TARGET_USES_QCOM_BSP_LEGACY)
 SOONG_CONFIG_corvusQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
 SOONG_CONFIG_corvusQcomVars_needs_camera_boottime_timestamp := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_corvusGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
