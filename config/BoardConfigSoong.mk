6428e58005a2c4425833c26890deff3d1f0ba32c
Michael Bestas <mkbestas@lineageos.org>
2022-09-12 22:57:57 +0530

    config: Remove TARGET_PROCESS_SDK_VERSION_OVERRIDE This hasn't been ported after lineage-18.1
    Change-Id: I3024e503e97075d5bfffc41b60da83a445a4e580

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -38,7 +38,6 @@ SOONG_CONFIG_corvusGlobalVars += \
     target_inputdispatcher_skip_event_key \
     target_ld_shim_libs \
     target_inputdispatcher_skip_event_key \
-    target_process_sdk_version_override \
     target_surfaceflinger_udfps_lib \
     target_uses_prebuilt_dynamic_partitions \
     camera_needs_client_info_lib \
@@ -89,7 +88,6 @@ SOONG_CONFIG_corvusGlobalVars_bootloader_message_offset := $(BOOTLOADER_MESSAGE_
 SOONG_CONFIG_corvusGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_corvusGlobalVars_target_inputdispatcher_skip_event_key := $(TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY)
 SOONG_CONFIG_corvusGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
-SOONG_CONFIG_corvusGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
 SOONG_CONFIG_corvusGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFACEFLINGER_UDFPS_LIB)
 SOONG_CONFIG_corvusGlobalVars_target_inputdispatcher_skip_event_key := $(TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY)
 ifneq ($(filter $(QSSI_SUPPORTED_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
